8c64de0548f5e4d21ec34407a2133cb1b59404ce
Franco Vega <franco_vega95@hotmail.com>
2020-07-14 14:25:34 -0300

    modificaciones

--- Controller.py
+++ Controller.py
@@ -2,9 +2,14 @@ from flask import render_template, redirect, url_for, request, json
 from App import app
 import hashlib
 
+# Rutas de la aplicacion
 @app.route('/')
 def login():
     return render_template('login/login.html')
+
+@app.route('/forgot-password')
+def forgot():
+    return render_template('forgot/forgot-pass.html')
 
 @app.route('/home')
 def home():
@@ -13,3 +18,7 @@ def home():
 @app.route('/register')
 def register():
     return render_template('register/register.html')
+
+@app.route('/register-user')
+def register_user():
+    return render_template('register/register-admin.html')
